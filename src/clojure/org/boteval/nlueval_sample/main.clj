e2f95155c4f3ddc57f049c1fd6b6ec2590333c16
matan <dev.matan@gmail.com>
2017-12-08 11:28:51 +0200

    added gen-class for enabling an uberjar

--- src/clojure/org/boteval/nlueval_sample/main.clj
+++ src/clojure/org/boteval/nlueval_sample/main.clj
@@ -1,7 +1,8 @@
 (ns org.boteval.nlueval-sample.main
   (:require
     [org.boteval.nlueval.input.ready :refer :all]
-    [org.boteval.nlueval-sample.execute :refer :all]))
+    [org.boteval.nlueval-sample.execute :refer :all])
+  (:gen-class)) ; for uberjar deploy to air-gapped environments
 
 (defn -main []
   (let [ready-data (ready-data :data-files)]
